6e772581facafe8f8a26d805bfcaca1c203f5923
jfernand <jfernand196@gmail.com>
2022-02-19 17:17:38 -0500

    Update: other solution with pug library

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const app = express();
 
+// OPTION 1 with pug library
 
-app.get("/", (req, res)=>{
-       let arr = []
-        for(let i = 1; i<=50; i++){
-            arr.push(`<p>${i} ${i%2==0? 'Soy Par!': 'Soy Impar!'}</p>`)
-        }
+app.set('view engine','pug')
+app.set('views','views')
 
-        res.send(arr.join(' '))
-
+app.get('/',(req,res)=>{
+    let arr =[]
+    for(let i = 1; i<=50; i++){
+        arr.push(`Soy ${ i%2==0? 'Par!':'Impar!'}`)
+    }
+    res.render('index',{text:arr});
 })
+
+
+
+// OPTION 2
+// app.get("/", (req, res)=>{
+//        let arr = []
+//         for(let i = 1; i<=50; i++){
+//             arr.push(`<p>${i} ${i%2==0? 'Soy Par!': 'Soy Impar!'}</p>`)
+//         }
+
+//         res.send(arr.join(' '))
+
+// })
 
 
 
